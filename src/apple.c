4e5f63bb0c32dd4e2354bd28373c9fdf45218464
hoangdinhtran1995 <51941188+hoangdinhtran1995@users.noreply.github.com>
2019-06-24 21:57:46 +0900

    Update apple.c

--- src/apple.c
+++ src/apple.c
@@ -4,32 +4,33 @@ int n;
 int k;
 int A[100000];
 
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-  // Find abundance of bags and who needs them
-  int b = k - n ; //b bags
-  int stop = n - b;
-  int i = n;
-  while (i > stop) {			
-	  int y = A[i] / b;
-	  if (y > A[i-1])
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
 
-	  
+	int max = 0;
+	for (i = 0; i < n; i++) {
+		if (A[i] > max)
+			max = A[i];
+	}
 
-  }
-
-
-  //while (ub - lb > 1) {
-	//  int m = (lb + ub) / 2;
-	  //if (A[m] < k)
-		//  lb = m;
-	 // else
-		//  ub = m;
-  //}
-  return 0;
+	lb = 0;
+	ub = max;
+	while (ub - lb > 1) {
+		int m = (lb + ub) / 2;
+		int i;
+		int d = 0;
+		for (i = 0; i < n; i++) {
+			d = d + (A[i] + m - 1) / m;
+		}
+		if (d <= k)
+			ub = m;
+		else lb = m;
+	}
+	printf("%d\n", ub);
+	return 0;
 }
