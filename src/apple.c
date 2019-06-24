161301b3de9c90371387ce702cf7aefc042b988a
hoangdinhtran1995 <51941188+hoangdinhtran1995@users.noreply.github.com>
2019-06-24 16:36:51 +0900

    Update apple.c

--- src/apple.c
+++ src/apple.c
@@ -11,7 +11,25 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  // Find abundance of bags and who needs them
+  int b = k - n ; //b bags
+  int stop = n - b;
+  int i = n;
+  while (i > stop) {			
+	  int y = A[i] / b;
+	  if (y > A[i-1])
+
+	  
+
+  }
 
 
+  //while (ub - lb > 1) {
+	//  int m = (lb + ub) / 2;
+	  //if (A[m] < k)
+		//  lb = m;
+	 // else
+		//  ub = m;
+  //}
   return 0;
 }
