10d04190b15410edd6ee8f0748720109beecf8bb
hoangdinhtran1995 <51941188+hoangdinhtran1995@users.noreply.github.com>
2019-08-02 09:40:41 +0900

    Update array.c
    update

--- src/array.c
+++ src/array.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
+
+int n;
+int k;
+int A[100000];
+
 int main() {
-
-	int n;
-	int k;
-	int A[100000];
-
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
